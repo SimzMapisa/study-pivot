d6ca31a0fb6d307ea449bc7a36421d2ae605832f
SimzMapisa <simbarashe795@gmail.com>
2025-01-16 16:12:19 +0000

    add clerk auth library and removed the auth0 one

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,7 @@
 export default async function Home() {
-	console.log('am I a server or a client?');
-	const data = await fetch('http://localhost:3000/api/hello').then((res) =>
-		res.json()
-	);
 	return (
 		<>
-			<code>
-				<pre>{JSON.stringify(data, null, 2)}</pre>
-			</code>
-			<h1 className='text-lg text-white'>we Welcome to Next</h1>
+			<h1 className='text-lg text-white'>Welcome to Next</h1>
 		</>
 	);
 }
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	rewrites: async () => {
+		return [
+			// {
+			// 	source: '/api/auth/:path',
+			// 	destination: '/api/auth/:path*',
+			// },
+			{
+				source: '/api/:path*',
+				destination: 'http://localhost:5000/api/:path*',
+			},
+		];
+	},
 };
 
 export default nextConfig;
